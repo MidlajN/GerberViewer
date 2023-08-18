e6f6e467fa56401541e3fd4194efccdeffe6f028
Muhammed Midlaj <midlaj4n@gmail.com>
2023-08-18 17:05:03 +0530

    Gerber Viewer - UI implementation : Cutomizable Task Bar Updation

--- index.js
+++ index.js
@@ -9,9 +9,88 @@ function initializeGerberToSVG(files) {
   // }
 }
 
+document.addEventListener('DOMContentLoaded', function() {
+  var tabsNewAnim = $('#navbar-animmenu');
+  var activeItemNewAnim = tabsNewAnim.find('.active');
+  var activeWidthNewAnimWidth = activeItemNewAnim.innerWidth();
+  var itemPosNewAnimLeft = activeItemNewAnim.position();
+  $(".hori-selector").css({
+      "left": itemPosNewAnimLeft.left + "px",
+      "width": activeWidthNewAnimWidth + "px"
+  });
 
-function toggleLayer(LayerId) {
+  $("#navbar-animmenu").on("click", "li", function(e) {
+      $('#navbar-animmenu ul li').removeClass("active");
+      $(this).addClass('active');
+      var activeWidthNewAnimWidth = $(this).innerWidth();
+      var itemPosNewAnimLeft = $(this).position();
+      $(".hori-selector").css({
+          "left": itemPosNewAnimLeft.left + "px",
+          "width": activeWidthNewAnimWidth + "px"
+      });
+  });
 
+  // add new Task to the Navbar list
+  const button = document.getElementById('addNewButton');
+
+  button.addEventListener('click', function() {
+    const addNewIndex = button.getAttribute('data-sds');
+    const indexInt = parseInt(addNewIndex);
+
+    button.setAttribute('data-sds', indexInt + 1);
+
+    if (indexInt + 1 < 10) {
+      createNewItem();
+    } else {
+      createNewItem();
+      button.style.display = 'none';
+    }
+  });
+
+  function createNewItem() {
+    const newItem = document.createElement('li');
+    const newLink = document.createElement('a');
+    const secondLink = document.createElement('a');
+    secondLink.style.paddingLeft = '0px';
+    secondLink.setAttribute('onclick', 'removeItem(this)');
+    newLink.innerHTML = '<i class="far fa-file"></i>New Item ';
+    secondLink.innerHTML = '<i class="fa-solid fa-circle-xmark fa-sm" style="position:relative; right:-8px"></i>';
+    newItem.appendChild(newLink);
+    newItem.appendChild(secondLink);
+
+    $('#navbar-animmenu ul li').removeClass("active");
+
+    const navbarList = document.querySelector('#navbar-animmenu ul');
+    navbarList.appendChild(newItem);
+
+    $(newItem).addClass('active');
+
+    const activeWidthNewAnimWidth = $(newItem).innerWidth();
+    const itemPosNewAnimLeft = $(newItem).position();
+    $(".hori-selector").css({
+        "left": itemPosNewAnimLeft.left + "px",
+        "width": activeWidthNewAnimWidth + "px"
+    });
+  }
+});
+
+
+function removeItem(item) {
+  console.log('item : ', item);
+  let addNewButton = document.getElementById('addNewButton');
+  let index = addNewButton.getAttribute('data-sds');
+  index = parseInt(index);
+  if (index === 10) {
+    console.log('index : ', index);
+    addNewButton.style.display = '';
+  }
+  addNewButton.setAttribute('data-sds', index - 1);
+  $(item.parentElement).remove();
+  
+}
+
+function toggleLayer(LayerId, index) {
+  console.log('LayerId : ', index);
   let stackTop = document.getElementById("mainTop");
   let stackBottom = document.getElementById("mainBottom");
   const topLayerElem = stackTop.getElementsByTagName("g");
@@ -22,6 +101,8 @@ function toggleLayer(LayerId) {
       const layerId = layer.getAttribute("id");
       if (layerId.endsWith(LayerId)) {
         layer.style.display = layer.style.display === "none" ? "block" : "none";
+        layerStatus[index] = index;
+        
       }
     }
   });
@@ -36,7 +117,28 @@ function toggleLayer(LayerId) {
 }
 
 
+function makeEditable(liElement) {
+  // Create an input element and set its value to the text content of the li element
+  var inputElement = document.createElement("input");
+  inputElement.value = liElement.textContent;
 
+  // Replace the li element's content with the input element
+  liElement.innerHTML = "";
+  liElement.appendChild(inputElement);
 
+  // Focus on the input element and select its content
+  inputElement.focus();
+  inputElement.select();
 
-
+  // When the input element loses focus, revert back to the li element with the updated value
+  inputElement.addEventListener("blur", function() {
+      liElement.textContent = inputElement.value;
+  });
+  
+  // When the Enter key is pressed, update the li element and remove the input element
+  inputElement.addEventListener("keyup", function(event) {
+      if (event.key === "Enter") {
+          liElement.textContent = inputElement.value;
+      }
+  });
+}
